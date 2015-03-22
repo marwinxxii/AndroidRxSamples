ca4605ee9dc3587b9f55213ee08ceaf0a02ff29e
Alexey Agapitov <alexey.agapitov@zoho.com>
2015-03-22 15:08:42 +0300

    EmailSample - add old way example.

--- app/src/main/java/com/github/marwinxxii/rxsamples/EmailSampleActivity.java
+++ app/src/main/java/com/github/marwinxxii/rxsamples/EmailSampleActivity.java
@@ -2,7 +2,9 @@ package com.github.marwinxxii.rxsamples;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.EditText;
@@ -15,18 +17,26 @@ import rx.functions.Func2;
 import rx.subjects.PublishSubject;
 
 public class EmailSampleActivity extends Activity {
+    private EditText mLoginText;
+    private Spinner mDomainSpinner;
+    private EditText mResultText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sample_email);
-        EditText loginText = (EditText) findViewById(R.id.login);
-        Spinner domainSpinner = (Spinner) findViewById(R.id.domain);
-        final EditText resultText = (EditText) findViewById(R.id.result);
-        resultText.setEnabled(false);
-
+        mLoginText = (EditText) findViewById(R.id.login);
+        mDomainSpinner = (Spinner) findViewById(R.id.domain);
+        mResultText = (EditText) findViewById(R.id.result);
+        mResultText.setEnabled(false);
+        setupFields();
+        //setupFieldsOldWay();
+    }
+    
+    private void setupFields() {
         Observable.combineLatest(
-          WidgetObservable.text(loginText, true),
-          observeSelect(domainSpinner),
+          WidgetObservable.text(mLoginText, false),//false - do not emit start value
+          observeSelect(mDomainSpinner),
 
           new Func2<OnTextChangeEvent, String, String>() {
               @Override
@@ -42,7 +52,7 @@ public class EmailSampleActivity extends Activity {
         ).subscribe(new Action1<String>() {
             @Override
             public void call(String email) {
-                resultText.setText(email);
+                mResultText.setText(email);
             }
         });
     }
@@ -50,7 +60,7 @@ public class EmailSampleActivity extends Activity {
     public static Observable<String> observeSelect(Spinner spinner) {
         final PublishSubject<String> selectSubject = PublishSubject.create();
         // for production code, unsubscribe, UI thread assertions are needed
-        // see WidgetObservable for example
+        // see WidgetObservable from rxandroid for example
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
@@ -64,5 +74,42 @@ public class EmailSampleActivity extends Activity {
         });
 
         return selectSubject;
+    }
+
+    private void setupFieldsOldWay() {
+        mLoginText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                onFieldUpdated();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+        mDomainSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                onFieldUpdated();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+    }
+    
+    private void onFieldUpdated() {
+        String login = mLoginText.getText().toString();
+        String domain = (String) mDomainSpinner.getSelectedItem();
+        if (TextUtils.isEmpty(login)) {
+            mResultText.setText("");
+        } else {
+            mResultText.setText(login + '@' + domain);
+        }
     }
 }
