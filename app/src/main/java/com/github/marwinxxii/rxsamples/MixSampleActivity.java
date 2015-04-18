bf69bb3e1c9b82e87717c8a467a53e085d7df4c8
Alexey Agapitov <alexey.agapitov@zoho.com>
2015-04-18 16:01:58 +0300

    Sample 3 - draft.

---
+++ app/src/main/java/com/github/marwinxxii/rxsamples/MixSampleActivity.java
@@ -0,0 +1,77 @@
+package com.github.marwinxxii.rxsamples;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+import rx.Observable;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.android.view.OnClickEvent;
+import rx.android.view.ViewObservable;
+import rx.functions.Action1;
+import rx.functions.Func1;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+public class MixSampleActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sample_mix);
+        final Button calculateBtn = (Button) findViewById(R.id.calculate);
+        final EditText result = (EditText) findViewById(R.id.result);
+        result.setEnabled(false);
+        final ProgressBar progress = (ProgressBar) findViewById(android.R.id.progress);
+
+        ViewObservable.clicks(calculateBtn)
+          .doOnNext(new Action1<OnClickEvent>() {
+              @Override
+              public void call(OnClickEvent onClickEvent) {
+                  calculateBtn.setEnabled(false);
+                  progress.setVisibility(View.VISIBLE);
+                  result.setText("");
+              }
+          })
+          .flatMap(new Func1<OnClickEvent, Observable<Integer>>() {
+              @Override
+              public Observable<Integer> call(OnClickEvent onClickEvent) {
+                  return observeLongApiCall()
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .doOnError(new Action1<Throwable>() {
+                        @Override
+                        public void call(Throwable throwable) {
+                            progress.setVisibility(View.GONE);
+                            calculateBtn.setEnabled(true);
+                            Toast.makeText(MixSampleActivity.this, R.string.mix_error_message, Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .onErrorResumeNext(Observable.<Integer>empty());
+              }
+          })
+          .subscribe(new Action1<Integer>() {
+              @Override
+              public void call(Integer integer) {
+                  progress.setVisibility(View.GONE);
+                  calculateBtn.setEnabled(true);
+                  result.setText(integer.toString());
+              }
+          });
+    }
+
+    private static Observable<Integer> observeLongApiCall() {
+        return Observable.just(new Random().nextInt())
+          .delay(3L, TimeUnit.SECONDS)
+          .doOnNext(new Action1<Integer>() {
+              @Override
+              public void call(Integer integer) {
+                  if (new Random().nextBoolean()) {
+                      throw new RuntimeException("Error calculating value");
+                  }
+              }
+          });
+    }
+}
